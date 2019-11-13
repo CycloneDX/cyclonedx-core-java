e3584ea199c443fba637b61c7751253fde17221a
Steve Springett <steve@springett.us>
2019-11-12 22:21:33 -0600

    Added schema extension support

--- src/main/java/org/cyclonedx/CycloneDxSchema.java
+++ src/main/java/org/cyclonedx/CycloneDxSchema.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
+ * SPDX-License-Identifier: Apache-2.0
  * Copyright (c) Steve Springett. All Rights Reserved.
  */
 package org.cyclonedx;
@@ -23,7 +24,14 @@ import javax.xml.transform.Source;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * CycloneDxSchema is a base class that provides schema information to
@@ -32,6 +40,8 @@ import java.io.InputStream;
  * @since 1.1.0
  */
 public abstract class CycloneDxSchema {
+
+    private static final Logger LOGGER = Logger.getLogger(CycloneDxSchema.class.getName());
 
     public static final String NS_BOM_10 = "http://cyclonedx.org/schema/bom/1.0";
     public static final String NS_BOM_11 = "http://cyclonedx.org/schema/bom/1.1";
@@ -91,10 +101,20 @@ public abstract class CycloneDxSchema {
      */
     private Schema getXmlSchema11() throws SAXException {
         // Use local copies of schemas rather than resolving from the net. It's faster, and less prone to errors.
-        return getXmlSchema(
-                this.getClass().getClassLoader().getResourceAsStream("spdx.xsd"),
-                this.getClass().getClassLoader().getResourceAsStream("bom-1.1.xsd")
-        );
+        final List<InputStream> streams = new ArrayList<>();
+        streams.add(this.getClass().getClassLoader().getResourceAsStream("spdx.xsd"));
+        streams.add(this.getClass().getClassLoader().getResourceAsStream("bom-1.1.xsd"));
+        // Automatically load all schema extensions from 'resources/ext' directory
+        try {
+            final Enumeration<URL> urls = this.getClass().getClassLoader().getResources("ext");
+            while (urls.hasMoreElements()) {
+                final URL url = urls.nextElement();
+                streams.add(url.openStream());
+            }
+        } catch (IOException e) {
+            LOGGER.log(Level.WARNING, "An error occurred attempting to load schema extensions", e);
+        }
+        return getXmlSchema(streams.toArray(new InputStream[0]));
     }
 
     public Schema getXmlSchema(InputStream... inputStreams) throws SAXException {
