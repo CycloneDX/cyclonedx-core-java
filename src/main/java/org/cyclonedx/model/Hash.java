03deea1cdc7d85f1d964ccd0fee880691c90fb7f
Steve Springett <steve@springett.us>
2020-07-05 21:39:57 -0500

    Added hash algorithms introduced in spec v1.2

--- src/main/java/org/cyclonedx/model/Hash.java
+++ src/main/java/org/cyclonedx/model/Hash.java
@@ -30,7 +30,12 @@ public class Hash {
         SHA_384("SHA-384"),
         SHA_512("SHA-512"),
         SHA3_256("SHA3-256"),
-        SHA3_512("SHA3-512");
+        SHA3_384("SHA3-384"),
+        SHA3_512("SHA3-512"),
+        BLAKE2b_256("BLAKE2b-256"),
+        BLAKE2b_384("BLAKE2b-384"),
+        BLAKE2b_512("BLAKE2b-512"),
+        BLAKE3("BLAKE3");
 
         private String spec;
         Algorithm(String spec) {
