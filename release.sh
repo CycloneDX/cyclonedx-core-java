67f0bc8712ac4214df9ebe6b3b764507a3eae8fb
Steve Springett <steve@springett.us>
2020-07-10 17:18:16 -0500

    Back to 1.8

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-export JAVA_HOME=`/usr/libexec/java_home -v 11.0`
+export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
 export PATH=JAVA_HOME/bin:$PATH
 
 read -p "Really deploy to Maven Central repository (Y/N)? "
