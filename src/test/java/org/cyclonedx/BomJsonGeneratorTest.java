e52ff8da5588e29260cb8a15b16b310904dc6080
Dan Rollo <danrollo@gmail.com>
2020-11-12 16:03:06 -0500

    add json schema for 'vulnerability' to existing bom json schema. need to extract this somehow.

--- src/test/java/org/cyclonedx/BomJsonGeneratorTest.java
+++ src/test/java/org/cyclonedx/BomJsonGeneratorTest.java
@@ -47,6 +47,7 @@ public class BomJsonGeneratorTest {
     @Test
     public void schema12GenerationWithVulnsTest() throws Exception {
         Bom bom =  createCommonBom("/bom-1.1-vulnerability-1.0.xml");
+        //Bom bom =  createCommonBom("/bom-1.2-vulnerability-1.0.xml");
         BomJsonGenerator generator = BomGeneratorFactory.createJson(CycloneDxSchema.Version.VERSION_12, bom);
         generator.generate();
         Assert.assertTrue(generator instanceof BomJsonGenerator12);
