025dfb8d4525aeb45e887c56b2da4f34c9115829
aalzate <aalzate@sonatype.com>
2022-01-26 22:25:23 -0500

    Initial test for XMLParser test 1.4
    Signed-off-by: aalzate <aalzate@sonatype.com>

--- src/test/java/org/cyclonedx/parsers/XmlParserTest.java
+++ src/test/java/org/cyclonedx/parsers/XmlParserTest.java
@@ -24,11 +24,19 @@ import org.cyclonedx.model.Bom;
 import org.cyclonedx.model.Component;
 import org.cyclonedx.model.Dependency;
 import org.cyclonedx.model.ExternalReference;
+import org.cyclonedx.model.Metadata;
 import org.cyclonedx.model.OrganizationalContact;
 import org.cyclonedx.model.OrganizationalEntity;
 import org.cyclonedx.model.Pedigree;
+import org.cyclonedx.model.ReleaseNotes;
+import org.cyclonedx.model.ReleaseNotes.Notes;
+import org.cyclonedx.model.ReleaseNotes.Resolves;
 import org.cyclonedx.model.Service;
 import org.cyclonedx.model.ServiceData;
+import org.cyclonedx.model.vulnerability.Vulnerability;
+import org.cyclonedx.model.vulnerability.Vulnerability.Rating;
+import org.cyclonedx.model.vulnerability.Vulnerability.Rating.Method;
+import org.cyclonedx.model.vulnerability.Vulnerability.Rating.Severity;
 import org.junit.jupiter.api.Test;
 import java.io.File;
 import java.util.List;
@@ -425,6 +433,228 @@ public class XmlParserTest {
         Dependency d11 = d1.getDependencies().get(0);
         assertEquals("pkg:maven/org.acme/web-framework@1.0.0", d11.getRef());
         assertNull(d11.getDependencies());
+    }
+
+    @Test
+    public void testParsedObjects14Bom() throws Exception {
+        final byte[] bomBytes = IOUtils.toByteArray(this.getClass().getResourceAsStream("/bom-1.4.xml"));
+        final XmlParser parser = new XmlParser();
+        final Bom bom = parser.parse(bomBytes);
+
+        assertEquals("1.4", bom.getSpecVersion());
+        assertEquals(1, bom.getVersion());
+
+        assertMetadata(bom.getMetadata());
+        assertComponent(bom);
+        assertServices(bom);
+        assertVulnerabilities(bom);
+
+        // Dependencies
+        assertEquals(1, bom.getDependencies().size());
+        Dependency d1 = bom.getDependencies().get(0);
+        assertNotNull(d1);
+        assertEquals("pkg:maven/com.acme/jackson-databind@2.9.4", d1.getRef());
+    }
+
+    private void assertVulnerabilities (final Bom bom) {
+
+        final List<Vulnerability> vulnerabilities = bom.getVulnerabilities();
+        assertEquals(1, vulnerabilities.size());
+        Vulnerability vuln = vulnerabilities.get(0);
+
+        assertEquals("6eee14da-8f42-4cc4-bb65-203235f02415", vuln.getBomRef());
+        assertEquals("SONATYPE-2021123", vuln.getId());
+        assertEquals("Description", vuln.getDescription());
+        assertEquals("Detail", vuln.getDetail());
+        assertEquals("Upgrade", vuln.getRecommendation());
+        assertEquals(184, vuln.getCwes().get(0));
+        assertNotNull(vuln.getCreated());
+        assertNotNull(vuln.getPublished());
+        assertNotNull(vuln.getUpdated());
+
+        //Source
+        assertEquals("Sonatype", vuln.getSource().getName());
+        assertEquals("https://www.vuln.com", vuln.getSource().getUrl());
+
+        //References
+        assertEquals(1, vuln.getReferences().size());
+        assertEquals("CVE-2018-7489", vuln.getReferences().get(0).getId());
+        assertEquals("NVD", vuln.getReferences().get(0).getSource().getName());
+        assertEquals("https://nvd.nist.gov/vuln/detail/CVE-2019-9997", vuln.getReferences().get(0).getSource().getUrl());
+
+        //Ratings
+        assertEquals(1, vuln.getRatings().size());
+        assertEquals("NVD", vuln.getRatings().get(0).getSource().getName());
+        assertEquals(
+            "https://nvd.nist.gov/vuln-metrics/cvss/v3-calculator?vector=AV:N/AC:L/PR:N/UI:N/S:U/C:H/I:H/A:H&version=3.0",
+            vuln.getRatings().get(0).getSource().getUrl());
+        assertEquals(9.8, vuln.getRatings().get(0).getScore());
+        assertEquals(Severity.CRITICAL, vuln.getRatings().get(0).getSeverity());
+        assertEquals(Method.CVSSV3, vuln.getRatings().get(0).getMethod());
+        assertEquals("AN/AC:L/PR:N/UI:N/S:U/C:H/I:H/A:H", vuln.getRatings().get(0).getVector());
+        assertEquals("An optional reason for rating the vulnerability as it was", vuln.getRatings().get(0).getJustification());
+
+        //Advisories
+        assertEquals(1, vuln.getAdvisories().size());
+        assertEquals("GitHub Commit", vuln.getAdvisories().get(0).getTitle());
+        assertEquals("https://github.com/FasterXML/jackson-databind/commit/6799f8f10cc78e9af6d443ed6982d00a13f2e7d2",
+            vuln.getAdvisories().get(0).getUrl());
+
+        //Credits
+        assertEquals(1, vuln.getCredits().getIndividuals().size());
+        assertEquals(1, vuln.getCredits().getOrganizations().size());
+
+        assertEquals("Jane Doe", vuln.getCredits().getIndividuals().get(0).getName());
+        //assertEquals("jane.doe@example.com", vuln.getCredits().getIndividuals().get(0).getExtensions().get("email"));
+
+        assertEquals("Acme, Inc.", vuln.getCredits().getOrganizations().get(0).getName());
+        // assertEquals("https://example.com", vuln.getCredits().getOrganizations().get(0).getExtensions().get("url"));
+    }
+
+    private void assertServices(final Bom bom) {
+        //Services
+        List<Service> services = bom.getServices();
+        assertEquals(1, services.size());
+        Service s = services.get(0);
+        OrganizationalEntity provider = s.getProvider();
+        assertNotNull(provider);
+        assertEquals("Partner Org", provider.getName());
+        List<String> urls = provider.getUrls();
+        assertEquals(1, urls.size());
+        assertEquals("https://partner.org", urls.get(0));
+        List<OrganizationalContact> contacts = provider.getContacts();
+        assertEquals(1, contacts.size());
+        OrganizationalContact contact = contacts.get(0);
+        assertEquals("Support", contact.getName());
+        assertEquals("support@partner", contact.getEmail());
+        assertEquals("800-555-1212", contact.getPhone());
+        assertEquals("org.partner", s.getGroup());
+        assertEquals("Stock ticker service", s.getName());
+        assertEquals("2020-Q2", s.getVersion());
+        assertEquals("Provides real-time stock information", s.getDescription());
+        List<String> endpoints = s.getEndpoints();
+        assertEquals(2, endpoints.size());
+        assertEquals("https://partner.org/api/v1/lookup", endpoints.get(0));
+        assertEquals("https://partner.org/api/v1/stock", endpoints.get(1));
+        assertNotNull(s.getAuthenticated());
+        assertNotNull(s.getxTrustBoundary());
+        assertTrue(s.getAuthenticated());
+        assertTrue(s.getxTrustBoundary());
+        List<ServiceData> data = s.getData();
+        assertEquals(4, data.size());
+        assertEquals("inbound", data.get(0).getFlow().getFlowName());
+        assertEquals("PII", data.get(0).getClassification());
+        assertEquals(ServiceData.Flow.OUTBOUND, data.get(1).getFlow());
+        assertEquals("PIFI", data.get(1).getClassification());
+        assertEquals(ServiceData.Flow.BI_DIRECTIONAL, data.get(2).getFlow());
+        assertEquals("pubic", data.get(2).getClassification());
+        assertNotNull(s.getLicense());
+        assertEquals(1, s.getLicense().getLicenses().size());
+        assertEquals("Partner license", s.getLicense().getLicenses().get(0).getName());
+        assertEquals(2, s.getExternalReferences().size());
+        assertEquals(ExternalReference.Type.WEBSITE, s.getExternalReferences().get(0).getType());
+        assertEquals("http://partner.org", s.getExternalReferences().get(0).getUrl());
+        assertEquals(ExternalReference.Type.DOCUMENTATION, s.getExternalReferences().get(1).getType());
+        assertEquals("http://api.partner.org/swagger", s.getExternalReferences().get(1).getUrl());
+    }
+
+    private void assertComponent(final Bom bom){
+        final List<Component> components = bom.getComponents();
+        assertEquals(1, components.size());
+        Component component = components.get(0);
+        assertEquals("com.acme", component.getGroup());
+        assertEquals("jackson-databind", component.getName());
+        assertEquals("2.9.4", component.getVersion());
+        assertEquals(Component.Type.LIBRARY, component.getType());
+        assertEquals("pkg:maven/com.acme/jackson-databind@2.9.4", component.getPurl());
+        assertEquals(12, component.getHashes().size());
+        assertNotNull(component.getLicenseChoice().getExpression());
+        assertEquals("Copyright Example Inc. All rights reserved.", component.getCopyright());
+        assertEquals("Acme Application", component.getSwid().getName());
+        assertEquals("9.1.1", component.getSwid().getVersion());
+        assertEquals("swidgen-242eb18a-503e-ca37-393b-cf156ef09691_9.1.1", component.getSwid().getTagId());
+        assertEquals(1, component.getExternalReferences().size());
+
+        //Evidence
+        assertNotNull(component.getEvidence());
+        assertEquals("Copyright 2012 Google Inc. All Rights Reserved.",  component.getEvidence().getCopyright().get(0).getText());
+        assertEquals("Apache-2.0",  component.getEvidence().getLicenseChoice().getLicenses().get(0).getId());
+        assertEquals("http://www.apache.org/licenses/LICENSE-2.0",  component.getEvidence().getLicenseChoice().getLicenses().get(0).getUrl());
+
+    }
+
+    private void assertMetadata(final Metadata metadata) {
+        assertNotNull(metadata);
+        assertNotNull(metadata.getTimestamp());
+
+        //Tool
+        assertEquals(1, metadata.getTools().size());
+        assertEquals("Awesome Vendor", metadata.getTools().get(0).getVendor());
+        assertEquals("Awesome Tool", metadata.getTools().get(0).getName());
+        assertEquals("9.1.2", metadata.getTools().get(0).getVersion());
+        assertEquals(1, metadata.getTools().get(0).getHashes().size());
+        assertEquals("SHA-1", metadata.getTools().get(0).getHashes().get(0).getAlgorithm());
+        assertEquals("25ed8e31b995bb927966616df2a42b979a2717f0",
+            metadata.getTools().get(0).getHashes().get(0).getValue());
+
+        //Author
+        assertEquals(1, metadata.getAuthors().size());
+        assertEquals("Samantha Wright", metadata.getAuthors().get(0).getName());
+        assertEquals("samantha.wright@example.com", metadata.getAuthors().get(0).getEmail());
+        assertEquals("800-555-1212", metadata.getAuthors().get(0).getPhone());
+
+        //Component
+        Component component = metadata.getComponent();
+        assertEquals("Acme Application", component.getName());
+        assertEquals("9.1.1", component.getVersion());
+        assertEquals(Component.Type.APPLICATION, component.getType());
+        assertNotNull(component.getSwid());
+        assertEquals("swidgen-242eb18a-503e-ca37-393b-cf156ef09691_9.1.1", component.getSwid().getTagId());
+        assertEquals("Acme Application", component.getSwid().getName());
+        assertEquals("9.1.1", component.getSwid().getVersion());
+        assertEquals(0, component.getSwid().getTagVersion());
+        assertFalse(component.getSwid().isPatch());
+
+        //Release Notes
+        ReleaseNotes releaseNotes = metadata.getComponent().getReleaseNotes();
+        assertEquals("major", releaseNotes.getType());
+        assertEquals("My new release", releaseNotes.getTitle());
+        assertNotNull(releaseNotes.getDescription());
+        assertNotNull(releaseNotes.getSocialImage());
+        assertNotNull(releaseNotes.getTimestamp());
+        assertEquals(1, releaseNotes.getAliases().size());
+        assertEquals(1, releaseNotes.getTags().size());
+
+        //Resolves
+        assertEquals(1, releaseNotes.getResolves().size());
+        Resolves resolves = releaseNotes.getResolves().get(0);
+        assertEquals(Resolves.Type.SECURITY, resolves.getType());
+        assertEquals("CVE-2019-9997", resolves.getId());
+        assertEquals("A security issue was fixed that did something bad", resolves.getDescription());
+        assertEquals("CVE-2019-9997", resolves.getName());
+
+        //Notes
+        assertEquals(1, releaseNotes.getNotes().size());
+        Notes note = releaseNotes.getNotes().get(0);
+        assertEquals("en-US", note.getLocale());
+        assertNotNull(note.getText());
+        assertEquals("PGgxPk15IG5ldyByZWxlYXNlPGgxPgo8cD5SZWxlYXNlIG5vdGVzIGhlcmU8L3A+", note.getText().getText());
+        assertEquals("text/html", note.getText().getContentType());
+        assertEquals("base64", note.getText().getEncoding());
+
+        //Manufacture
+        assertEquals("Acme, Inc.", metadata.getManufacture().getName());
+        assertEquals("https://example.com", metadata.getManufacture().getUrls().get(0));
+        assertEquals(1, metadata.getManufacture().getContacts().size());
+        assertEquals("Acme Professional Services", metadata.getManufacture().getContacts().get(0).getName());
+        assertEquals("professional.services@example.com", metadata.getManufacture().getContacts().get(0).getEmail());
+        assertEquals("Acme, Inc.", metadata.getSupplier().getName());
+
+        //Supplier
+        assertEquals("https://example.com", metadata.getSupplier().getUrls().get(0));
+        assertEquals(1, metadata.getSupplier().getContacts().size());
+        assertEquals("Acme Distribution", metadata.getSupplier().getContacts().get(0).getName());
+        assertEquals("distribution@example.com", metadata.getSupplier().getContacts().get(0).getEmail());
     }
 
     @Test
