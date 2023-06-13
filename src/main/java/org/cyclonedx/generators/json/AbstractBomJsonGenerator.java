3beca46311ee4237148dd67c2ebf4c24c1fab731
Alex Alzate <aalzate@sonatype.com>
2023-06-13 11:54:43 -0500

    Fix serialization
    Signed-off-by: Alex Alzate <aalzate@sonatype.com>

--- src/main/java/org/cyclonedx/generators/json/AbstractBomJsonGenerator.java
+++ src/main/java/org/cyclonedx/generators/json/AbstractBomJsonGenerator.java
@@ -27,6 +27,7 @@ import org.cyclonedx.model.BomReference;
 import org.cyclonedx.util.serializer.ComponentWrapperSerializer;
 import org.cyclonedx.util.serializer.LicenseChoiceSerializer;
 import org.cyclonedx.util.serializer.TrimStringSerializer;
+import org.cyclonedx.util.LifecycleSerializer;
 import org.cyclonedx.util.VersionJsonAnnotationIntrospector;
 import org.cyclonedx.util.serializer.DependencySerializer;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -72,8 +73,11 @@ public abstract class AbstractBomJsonGenerator extends CycloneDxSchema implement
         mapper.registerModule(stringModule);
 
         licenseModule.addSerializer(new LicenseChoiceSerializer());
-
         mapper.registerModule(licenseModule);
+
+        SimpleModule lifecycleModule = new SimpleModule();
+        lifecycleModule.addSerializer(new LifecycleSerializer(false));
+        mapper.registerModule(lifecycleModule);
 
         depModule.addSerializer(new DependencySerializer(false));
         mapper.registerModule(depModule);
