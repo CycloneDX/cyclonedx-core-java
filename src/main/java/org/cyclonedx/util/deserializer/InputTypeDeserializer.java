391ea57393f74eff7451071ed7e6e517e0c22002
Alex Alzate <aalzate@sonatype.com>
2024-05-27 08:06:40 -0500

    Fix issues with Env Variables serialization and deserialization

--- src/main/java/org/cyclonedx/util/deserializer/InputTypeDeserializer.java
+++ src/main/java/org/cyclonedx/util/deserializer/InputTypeDeserializer.java
@@ -25,11 +25,18 @@ import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import org.cyclonedx.model.AttachmentText;
+import org.cyclonedx.model.Property;
+import org.cyclonedx.model.formulation.common.EnvironmentVars;
 import org.cyclonedx.model.formulation.common.InputType;
 import org.cyclonedx.model.formulation.common.InputType.Parameter;
 
 public class InputTypeDeserializer extends AbstractDataTypeDeserializer<InputType> {
+  private final ObjectMapper objectMapper = new ObjectMapper();
+
+  private final EnvironmentVarsDeserializer environmentVarsDeserializer = new EnvironmentVarsDeserializer();
 
   @Override
   public InputType deserialize(JsonParser jsonParser, DeserializationContext deserializationContext)
