a2d28f9f761ec49182d831cc761e662943767485
Sebastian Schuberth <sschuberth@gmail.com>
2024-07-11 11:08:16 +0200

    Add the media type to the `Format` enum
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- src/main/java/org/cyclonedx/Format.java
+++ src/main/java/org/cyclonedx/Format.java
@@ -1,13 +1,15 @@
 package org.cyclonedx;
 
 public enum Format {
-    XML("xml"),
-    JSON("json");
+    XML("xml", "application/vnd.cyclonedx+xml"),
+    JSON("json", "application/vnd.cyclonedx+json");
 
     private final String extension;
+    private final String mediaType;
 
-    Format(String extension) {
+    Format(String extension, String mediaType) {
         this.extension = extension;
+        this.mediaType = mediaType;
     }
 
     /**
@@ -17,5 +19,15 @@ public enum Format {
      */
     public String getExtension() {
         return extension;
+    }
+
+    /**
+     * The official CycloneDX media type assigned by IANA for this format, see
+     * https://cyclonedx.org/specification/overview/#registered-media-types.
+     *
+     * @return The identifier for the media type.
+     */
+    public String getMediaType() {
+        return mediaType;
     }
 }
