08e03f87e489973ed2069633c7c65417aec100b7
Alex Alzate <aalzate@sonatype.com>
2024-09-07 11:07:16 -0500

    Move classes from package

--- src/main/java/org/cyclonedx/model/attestation/evidence/Data.java
+++ src/main/java/org/cyclonedx/model/attestation/evidence/Data.java
@@ -8,7 +8,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
-import org.cyclonedx.model.component.modelCard.data.Governance;
+import org.cyclonedx.model.component.data.Governance;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
