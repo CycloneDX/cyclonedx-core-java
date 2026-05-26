775fe4009d761d8ebac9f05f49df9adce3332a97
Sebastian Tiemann <setie@mailbox.org>
2026-05-26 09:38:45 +0300

    revert chnage to bom reference JSON property
    Signed-off-by: Sebastian Tiemann <setie@mailbox.org>

--- src/main/java/org/cyclonedx/model/Citation.java
+++ src/main/java/org/cyclonedx/model/Citation.java
@@ -40,7 +40,7 @@ import java.util.Objects;
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(Include.NON_NULL)
-@JsonPropertyOrder({"bomRef", "pointers", "expressions", "timestamp", "attributedTo", "process", "note", "signature"})
+@JsonPropertyOrder({"bom-ref", "pointers", "expressions", "timestamp", "attributedTo", "process", "note", "signature"})
 public class Citation extends ExtensibleElement {
 
     @JacksonXmlProperty(isAttribute = true, localName = "bom-ref")
